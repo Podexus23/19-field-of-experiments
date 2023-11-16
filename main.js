ad62f156606c995b53e778b1e52c3ffbc4814603
Podexus23 <santon23@gmail.com>
2023-11-16 21:00:25 +0300

    feat: check hugged body parts

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
+const hugButton = document.querySelector(".fight-btn");
+const form = document.forms.namedItem("fighters-form");
 
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const playerOne = form.querySelector(".fighters-player1");
+  const playerTwo = form.querySelector(".fighters-player2");
+  const bodyPartOne = Array.from(
+    playerOne.querySelectorAll(`input[type = "radio"]`)
+  ).filter((part) => part.checked)[0].value;
+  const bodyPartTwo = Array.from(
+    playerTwo.querySelectorAll(`input[type = "radio"]`)
+  ).filter((part) => part.checked)[0].value;
+
+  console.log(bodyPartOne, bodyPartTwo);
+});
