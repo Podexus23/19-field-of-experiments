22ff6547c7344de6e09065f5401a912ad84da4d8
Podexus23 <santon23@gmail.com>
2023-11-18 23:21:47 +0300

    feat: add fight log

--- src/main.js
+++ src/main.js
@@ -3,83 +3,140 @@ const announcementPlace = document.querySelector(".battlefield-announcement");
 const form = document.forms.namedItem("fighters-form");
 const playerOne = form.querySelector(".fighters-player1");
 const playerTwo = form.querySelector(".fighters-player2");
-const playerOneHealth = playerOne.querySelector(".player-1__health");
-const playerTwoHealth = playerTwo.querySelector(".player-2__health");
+const playerOneHealth = playerOne.querySelector(".player-health");
+const playerTwoHealth = playerTwo.querySelector(".player-health");
 const fullBodyOne = Array.from(
-  playerOne.querySelectorAll(`input[type = "radio"]`)
+  playerOne.querySelectorAll(`input[type = "radio"]`),
 );
 const fullBodyTwo = Array.from(
-  playerTwo.querySelectorAll(`input[type = "radio"]`)
+  playerTwo.querySelectorAll(`input[type = "radio"]`),
 );
 
-const hitNumbers = [3, 0, 1]; // for now =)
+let fighters = [];
 
-const fightMoveHandler = (e) => {
-  e.preventDefault();
-  console.log(partOne, partTwo);
+const createNewFighter = function (name = "fighter", stats) {
+  return {
+    name,
+    maxHealth: 100,
+    health: 100,
+  };
 };
 
-const hitForceCount = (max, min, modifier) => {
-  return (min + Math.random() * (max - min + 1)) * modifier;
+const nextStart = () => {
+  //add fighters
+  fighters.push(createNewFighter("Player 1"));
+  fighters.push(createNewFighter("Player 2"));
+  //put initial health on screen
+  playerOneHealth.querySelector(".health-is").textContent = fighters[0].health;
+  playerOneHealth.querySelector(".health-max").textContent = fighters[0].health;
+  playerTwoHealth.querySelector(".health-is").textContent = fighters[1].health;
+  playerTwoHealth.querySelector(".health-max").textContent = fighters[1].health;
+};
+
+window.addEventListener("load", nextStart);
+
+//max hit, min hit, modifier(use later)
+const hitNumbers = [0, 30, 1]; // for now =)
+const hitNumbers2 = [2, 4, 2]; // for now =)
+
+const hitForceCount = (min, max, modifier) => {
+  const base = min + Math.random() * (max - min) * modifier;
+  const modifiedBase = base;
+  return Math.ceil(modifiedBase);
 };
 
 const activateEndGame = () => {
-  //reset announcer
-  announcementPlace.textContent = "";
+  //reset announcer and fighters container
+  announcementPlace.textContent = "Let's begin";
+  fighters = [];
   //reset checkboxes
   fullBodyOne.forEach((checkbox) => (checkbox.checked = false));
   fullBodyTwo.forEach((checkbox) => (checkbox.checked = false));
-  //renew HP for each player
-  playerOneHealth.textContent = 10;
-  playerTwoHealth.textContent = 10;
+  //renew data for each fighter
+  nextStart();
   //block hug button
   hugButton.textContent = "hug!";
   hugButton.disabled = true;
 };
 
+const hitLogger = () => {
+  const logWindow = document.querySelector(".battle-log");
+  const {
+    name: name1,
+    maxHealth: maxHp1,
+    health: hp1,
+    lastTakenDamage: dmgTaken1,
+  } = fighters[0];
+  const {
+    name: name2,
+    maxHealth: maxHp2,
+    health: hp2,
+    lastTakenDamage: dmgTaken2,
+  } = fighters[1];
+  const firstFighterLog = `<div class="text-sm leading-3">
+    <span class="font-bold text-green-700">${name1} (${hp1}/${maxHp1}):</span>
+    <span>-${dmgTaken1}</span>
+   </div>
+  `;
+  const secondFighterLog = `<div class="text-sm leading-3">
+  <span class="font-bold text-red-700">${name2} (${hp2}/${maxHp2}):</span>
+  <span>-${dmgTaken2}</span>
+  </div>
+  `;
+  const oneRoundMsg = `<div class="p-1 pb-0">${secondFighterLog}${firstFighterLog}</div>`;
+  logWindow.insertAdjacentHTML("beforeend", oneRoundMsg);
+
+  console.log(logWindow.scrollTop);
+  console.dir(logWindow);
+  logWindow.scrollTop = logWindow.scrollHeight - logWindow.clientHeight;
+};
+
 const handleFightMove = function () {
-  let partOne, partTwo;
+  form.addEventListener("change", (e) => {
+    const partOne = fullBodyOne.filter((part) => part.checked)[0]?.value;
+    const partTwo = fullBodyTwo.filter((part) => part.checked)[0]?.value;
+    if (partOne && partTwo) hugButton.disabled = false;
+  });
 
   const fightMoveHandler = (e) => {
     e.preventDefault();
-    //remove HP on each of players
-    const firstPlayerHitForce = hitForceCount(...hitNumbers).toFixed(2);
-    const secondPlayerHitForce = hitForceCount(...hitNumbers).toFixed(2);
-    let playerOneHP = +playerOneHealth.textContent;
-    let playerTwoHP = +playerTwoHealth.textContent;
-    playerOneHP -= +firstPlayerHitForce;
-    playerTwoHP -= +secondPlayerHitForce;
-    playerOneHealth.textContent = playerOneHP.toFixed(2);
-    playerTwoHealth.textContent = playerTwoHP.toFixed(2);
+    //count damage
+    const firstPlayerHitForce = +hitForceCount(...hitNumbers);
+    const secondPlayerHitForce = +hitForceCount(...hitNumbers);
+    //show damage
+
+    fighters[0].health -= secondPlayerHitForce;
+    fighters[1].health -= firstPlayerHitForce;
+    fighters[0].lastTakenDamage = secondPlayerHitForce;
+    fighters[1].lastTakenDamage = firstPlayerHitForce;
+    //show info about round
+
+    playerOneHealth.querySelector(".health-is").textContent =
+      fighters[0].health;
+    playerTwoHealth.querySelector(".health-is").textContent =
+      fighters[1].health;
+
+    hitLogger();
     //endgame check
-    if (playerOneHP <= 0 && playerTwoHP <= 0) {
+    if (fighters[0].health <= 0 && fighters[1].health <= 0) {
       announcementPlace.textContent = "Draw";
       hugButton.textContent = "restart";
       return;
     }
-    if (playerOneHP <= 0 || playerTwoHP <= 0) {
+    if (fighters[0].health <= 0 || fighters[1].health <= 0) {
       announcementPlace.textContent = `Won player ${
-        playerOneHP > playerTwoHP ? "1" : "2"
+        fighters[0].health > fighters[1].health ? "1" : "2"
       }`;
       hugButton.textContent = "restart";
       return;
     }
     //continue game
-
-    //clean hit checkboxes
     fullBodyOne.forEach((checkbox) => (checkbox.checked = false));
     fullBodyTwo.forEach((checkbox) => (checkbox.checked = false));
-    //disable hug button
     hugButton.disabled = true;
   };
 
   form.addEventListener("submit", fightMoveHandler);
-
-  form.addEventListener("change", (e) => {
-    partOne = fullBodyOne.filter((part) => part.checked)[0]?.value;
-    partTwo = fullBodyTwo.filter((part) => part.checked)[0]?.value;
-    if (partOne && partTwo) hugButton.disabled = false;
-  });
 };
 
 hugButton.addEventListener("click", (e) => {
@@ -89,4 +146,5 @@ hugButton.addEventListener("click", (e) => {
 let init = function () {
   handleFightMove();
 };
+
 init();
